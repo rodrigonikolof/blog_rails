b4b7af43c4ce4566fdde583fef67fee5581c7f21
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-03-16 12:55:13 +1100

    Added user authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "email@email.com",  password: "password", password_confirmation: "password")
+
+10.times do |x|
+    Post.create(title: "Title #{x}", body: "Words go here #{x} and here are some more words", user_id: User.first.id)
+end
